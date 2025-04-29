b343da4054d83ae094f3aee4ea6345df15cd5bcd
kuzamma <jounotan@usm.edu.ph>
2025-04-29 08:33:55 +0800

    fix

--- app.py
+++ app.py
@@ -182,9 +182,4 @@ def predict():
 
 # === Main Start ===
 if __name__ == '__main__':
-    app.run(
-        host='0.0.0.0',
-        port=int(os.environ.get('PORT', 8080)),
-        threaded=True,
-        debug=False
-    )
+    app.run(host='0.0.0.0', port=8080, debug=True)
